f5ff227a878a26c5c27972955fead01470780870
SaWitC <ozabello21@gmail.com>
2022-12-26 10:36:22 +0100

    test2

--- BotServer/BotServer/Program.cs
+++ BotServer/BotServer/Program.cs
@@ -58,6 +58,7 @@ public class Program
                     //c.RoutePrefix = string.Empty;
                 });
             }
+            //asdasd
             app.UseProblemDetails();
             app.UseRouting();
             app.UseCors("MyAllowSpecificOrigins");
